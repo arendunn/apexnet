6403957d2233db71546d9fb1d0aaf08860ae67dc
Aren Dunn <arendunn@hotmail.com>
2025-06-05 09:38:18 +1000

    Fixed navigation buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ function goToContactPage() {
 }
 
 function goToServicesPage() {
-    window.location.href = "index.html";
+    window.location.href = "services.html";
 }
 
 function goToHomePage() {
