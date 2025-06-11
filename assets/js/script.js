426e3427e87290631f371418e3b283787e1c28d2
Aren Dunn <arendunn@hotmail.com>
2025-06-11 16:56:52 +1000

    Adjusted fade in threshold

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const faders = document.querySelectorAll('.fade-in');
 
     const appearOptions = {
-      threshold: 0.4,  // 10% visible
+      threshold: 0.2,  // 10% visible
       rootMargin: "0px 0px -50px 0px"  // trigger slightly before fully visible
     };
 
