75c694ead3ba2e9a0403da8f7d10ab038bd9a963
Aren Dunn <arendunn@hotmail.com>
2025-06-11 16:54:58 +1000

    Added fade in effec

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const faders = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+      threshold: 0.4,  // 10% visible
+      rootMargin: "0px 0px -50px 0px"  // trigger slightly before fully visible
+    };
+
+    const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          appearOnScroll.unobserve(entry.target); // animate once only
+        }
+      });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+      appearOnScroll.observe(fader);
+    });
+  });
+
 function goToServices() {
     const services = document.getElementById("services");
     if (services) {
