d0a87f033ee60adfbf728416a5e5b2b1c7b817e4
Aren Dunn <arendunn@hotmail.com>
2025-06-11 15:51:41 +1000

    New Services Layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,3 @@
-// Check if we are on GitHub Pages
-const isGitHubPages = window.location.hostname === 'arendunn.github.io';
-
-// Select all anchor tags in the page
-const links = document.querySelectorAll('a');
-
-// Loop through each link and adjust the href if on GitHub Pages
-links.forEach(link => {
-    const href = link.getAttribute('href');
-
-    // If on GitHub Pages and the link is not a full URL (doesn't start with http:// or https://)
-    if (isGitHubPages && !href.startsWith('http') && !href.startsWith('//')) {
-        // If the link is a relative path, add '/arendunn-portfolio/' to it
-        if (!href.startsWith('/')) {
-            link.href = '/apexnet/' + href;
-        }
-    }
-});
-
 function goToServices() {
     const services = document.getElementById("services");
     if (services) {
